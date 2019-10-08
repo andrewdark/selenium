2cede9b42105db5e6054cc135f262b7f649792ed
andrewdark <samsonov.a@ukr.net>
2019-10-08 17:57:14 +0300

    lesson 3. Task 7. Checks for a title for each page.

--- lesson_3/src/test/java/darknsoft/pp/ua/TestAdminMenu.java
+++ lesson_3/src/test/java/darknsoft/pp/ua/TestAdminMenu.java
@@ -4,10 +4,13 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertTrue;
 
 public class TestAdminMenu {
     private WebDriver webDriver;
@@ -24,24 +27,15 @@ public class TestAdminMenu {
         int outerSize = webDriver.findElements(By.cssSelector("ul#box-apps-menu>li")).size();
         for (int i = 0; i < outerSize; i++) {
             webDriver.findElements(By.cssSelector("ul#box-apps-menu>li")).get(i).click();
-            System.out.println(webDriver.findElement(By.tagName("title")).getAttribute("text"));
+            assertTrue(isElementPresent(webDriver, By.tagName("title")));
             int innerSize = webDriver.findElements(By.cssSelector("ul.docs>li")).size();
-            System.out.println("Menu# " + (i + 1) + " :" + innerSize);
             if (innerSize > 0) {
                 for (int j = 0; j < innerSize; j++) {
                     webDriver.findElements(By.cssSelector("ul.docs>li")).get(j).click();
-                    System.out.println((j + 1) + ") " + (webDriver.findElement(By.tagName("title")).getAttribute("text")).trim());
+                    assertTrue(isElementPresent(webDriver, By.tagName("title")));
                 }
             }
         }
-    }
-
-    private void loginAdminPage() {
-        webDriver.get("http://192.168.1.15/admin/login.php?redirect_url=%2Fadmin%2F");
-        webDriver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
-        webDriver.findElement(By.cssSelector("input[name=username]")).sendKeys("admin");
-        webDriver.findElement(By.cssSelector("input[name=password]")).sendKeys("admin");
-        webDriver.findElement(By.cssSelector("button[name=login]")).click();
     }
 
     @After
@@ -49,5 +43,21 @@ public class TestAdminMenu {
         webDriver.quit();
         webDriver = null;
     }
+    
+    private boolean isElementPresent(WebDriver driver, By locator) {
+        try {
+            driver.findElement(locator);
+            return true;
+        } catch (NoSuchElementException ex) {
+            return false;
+        }
+    }
 
+    private void loginAdminPage() {
+        webDriver.get("http://192.168.1.15/admin/login.php");
+        webDriver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+        webDriver.findElement(By.cssSelector("input[name=username]")).sendKeys("admin");
+        webDriver.findElement(By.cssSelector("input[name=password]")).sendKeys("admin");
+        webDriver.findElement(By.cssSelector("button[name=login]")).click();
+    }
 }
